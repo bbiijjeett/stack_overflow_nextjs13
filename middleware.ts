eefad868d6c0b615bd0f1f8bc05959ff3c15fb2b
bbiijjeett <bijeetnathbtech@gmail.com>
2024-02-29 00:45:40 +0530

    Bug fixed5

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/webhook",
+    "/api/webhooks(.*)",
     "question/:id",
     "/tags",
     "/tags/:id",
@@ -15,5 +15,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
