12fd00f546e0dc9469fd7b3680a08c485d08fd2f
bbiijjeett <bijeetnathbtech@gmail.com>
2024-03-30 14:47:27 +0530

    bug fiexed

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -38,7 +38,9 @@ const Question = ({ type, mongoUserId, questionDetails }: Props) => {
   const parsedQuestionDetails =
     questionDetails && JSON.parse(questionDetails || "");
 
-  const groupedTags = parsedQuestionDetails?.tags.map((tag) => tag.name);
+  const groupedTags = parsedQuestionDetails?.tags.map(
+    (tag: { name: any }) => tag.name
+  );
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
